6b79f7c422768f7df935e1a149050d348b17b88f
Max Velasco <max.velasco.rajo@gmail.com>
2025-07-10 00:39:12 +0200

    fix: PostCSS config for Tailwind v4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,6 @@
-// postcss.config.mjs
-import tailwind from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
 export default {
-  plugins: [
-    tailwind(),
-    autoprefixer(),
-  ],
-}
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
